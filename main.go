64a118befe73b9d433678ba60b9246b66aa74cf9
James Edward Royals <JER216L@c02v722mhtd6mbp.dv9119.homedepot.com>
2018-03-26 09:19:31 -0400

    set up server and testing

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+	return r
+}
+
+func main() {
+
+	r := newRouter()
+	http.ListenAndServe(":8000", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
